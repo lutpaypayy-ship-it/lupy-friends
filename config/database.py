8e306d544f30485aa4ca2763342aa35fe7aa05d2
lutpaypayy-ship-it <lutpaypayy@gmail.com>
2026-09-05 15:01:35 +0700

    Update kredensial Railway

--- config/database.py
+++ config/database.py
@@ -3,11 +3,11 @@ import mysql.connector
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host="mysql-1b358a98-lutpaypayy-2e7e.b.aivencloud.com",
-            user="avnadmin",
-            password="AVNS_btHCuD0GjjPDe9BoD-4",
-            database="defaultdb",
-            port=15414,
+            host="mysql.railway.internal",
+            user="root",
+            password="ptBdbHXWMXipHmeizKhOEcVacZvyGTHr",
+            database="railway",
+            port=3306,
             ssl_disabled=False  # Opsional: pastikan SSL aktif
         )
         return conn
