54d1b2ed897af4b1d52046a016f1d1ac3ebb8ccb
lutpaypayy-ship-it <lutpaypayy@gmail.com>
2026-09-05 15:07:09 +0700

    Fix database config ke Railway

--- config/database.py
+++ config/database.py
@@ -4,13 +4,12 @@ def get_db_connection():
     try:
         conn = mysql.connector.connect(
             host="mysql.railway.internal",
-            user="root",
+            user="root",  # atau isi dari MYSQLUSER Railway
             password="ptBdbHXWMXipHmeizKhOEcVacZvyGTHr",
-            database="railway",
-            port=3306,
-            ssl_disabled=False  # Opsional: pastikan SSL aktif
+            database="railway",  # atau isi dari MYSQLDATABASE Railway
+            port=3306  # ganti dengan angka dari MYSQLPORT Railway
         )
         return conn
     except Exception as e:
-        print(f"Error Koneksi Database Cloud: {e}")
+        print(f"Error Koneksi Database Railway: {e}")
         return None
